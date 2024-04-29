ac9398a222e7d6090e440c6c4dfaad3380681680
Gustavo Gondin Moeska <gumoeska@outlook.com>
2024-04-29 07:59:14 -0300

    Dev

---
+++ WinglyShop.Application/Cart/AddProductCartRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinglyShop.Application.Cart;
+
+public record AddProductCartRequest(int cartId, int productId, int quantity);
