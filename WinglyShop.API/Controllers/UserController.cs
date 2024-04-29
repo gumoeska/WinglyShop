557259e9803a71a6e69b17cfd9e62d7ba8d7333e
Gustavo Gondin Moeska <gumoeska@outlook.com>
2024-04-28 21:28:15 -0300

    Dev

---
+++ WinglyShop.API/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WinglyShop.API.Abstractions;
+using WinglyShop.Application.Abstractions.Data;
+using WinglyShop.Application.User.Create;
+using WinglyShop.Application.User.Update;
+using WinglyShop.Domain.Entities.User;
+using WinglyShop.Shared;
+
+namespace WinglyShop.API.Controllers;
+
+[Route("api/[controller]")]
+public class UserController : ApiController
+{
+	protected UserController(IDbConnection dbConnection) 
+		: base(dbConnection)
+	{
+	}
+
+	[HttpPost("CreateUser")]
+	public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request, CancellationToken cancellationToken)
+	{
+		// Validation
+		if (request is null)
+			return null;
+
+		var user = new User(
+			request.accountId, 
+			request.name, 
+			request.surname, 
+			request.image);
+
+		var command = new CreateUserCommand(user);
+
+		//Result<string> tokenResult = await _dispatcher.Send<LoginCommand, string>(command, cancellationToken);
+
+		//if (tokenResult.IsFailure)
+		//	return null;
+
+		Result<string> tokenResult = "Test";
+
+		return Ok(tokenResult);
+	}
+
+	[HttpPut("UpdateUser")]
+	public async Task<IActionResult> UpdateUser([FromBody] UpdateUserRequest request, CancellationToken cancellationToken)
+	{
+		// Validation
+		if (request is null)
+			return null;
+
+		var user = new User(
+			request.accountId,
+			request.name,
+			request.surname,
+			request.image);
+
+		var command = new UpdateUserCommand(user);
+
+		//Result<string> tokenResult = await _dispatcher.Send<LoginCommand, string>(command, cancellationToken);
+
+		//if (tokenResult.IsFailure)
+		//	return null;
+
+		Result<string> tokenResult = "Test";
+
+		return Ok(tokenResult);
+	}
+}
--- WinglyShop.Domain/Common/BaseEntity.cs
+++ WinglyShop.Domain/Common/BaseEntity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WinglyShop.Domain.Common.Interfaces;
+﻿using WinglyShop.Domain.Common.Interfaces;
 
 namespace WinglyShop.Domain.Common;
 
