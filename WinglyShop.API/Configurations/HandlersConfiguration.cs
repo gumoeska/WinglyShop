f7433a5e7f7acc1fbc576c3999ecd94c4eb73384
Gustavo Gondin Moeska <gumoeska@outlook.com>
2024-09-22 22:00:52 -0300

    refactor: Melhora o método de busca e instancia dos handlers

--- WinglyShop.API/Configurations/HandlersConfiguration.cs
+++ WinglyShop.API/Configurations/HandlersConfiguration.cs
@@ -5,39 +5,20 @@ namespace WinglyShop.API.Configurations;
 
 public static class HandlersConfiguration
 {
-	public static void AddHandlersFromAssembly(this IServiceCollection services, Assembly assembly)
-	{
-		#region Command Types
-		var commandHandlerInterfaceType = typeof(ICommandHandler<>);
-		var commandHandlerResponseInterfaceType = typeof(ICommandHandler<,>);
-		#endregion
+    public static void AddHandlersFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var handlerTypes = assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract)
+            .SelectMany(t => t.GetInterfaces(), (type, handlerInterface) => new { type, handlerInterface })
+            .Where(t => t.handlerInterface.IsGenericType &&
+                       (t.handlerInterface.GetGenericTypeDefinition() == typeof(ICommandHandler<>) ||
+                        t.handlerInterface.GetGenericTypeDefinition() == typeof(ICommandHandler<,>) ||
+                        t.handlerInterface.GetGenericTypeDefinition() == typeof(IQueryHandler<,>)))
+            .ToList();
 
-		#region Query Types
-		var queryHandlerInterfaceType = typeof(IQueryHandler<,>);
-		#endregion
-
-		var handlerTypes = assembly.GetTypes()
-			.Where(t => t.Namespace != null && t.Namespace.StartsWith("WinglyShop.Application"))
-			.Where(t => t.Name.EndsWith("Handler"))
-			.Where(t => t.GetInterfaces().Any(i =>
-				i.IsGenericType &&
-				(i.GetGenericTypeDefinition() == commandHandlerInterfaceType ||
-				 i.GetGenericTypeDefinition() == commandHandlerResponseInterfaceType ||
-				 i.GetGenericTypeDefinition() == queryHandlerInterfaceType)))
-			.ToList();
-
-		foreach (var handlerType in handlerTypes)
-		{
-			var implementedInterfaces = handlerType.GetInterfaces().Where(i =>
-				i.IsGenericType &&
-				(i.GetGenericTypeDefinition() == commandHandlerInterfaceType ||
-				 i.GetGenericTypeDefinition() == commandHandlerResponseInterfaceType ||
-				 i.GetGenericTypeDefinition() == queryHandlerInterfaceType));
-
-			foreach (var implementedInterface in implementedInterfaces)
-			{
-				services.AddTransient(implementedInterface, handlerType);
-			}
-		}
-	}
+        foreach (var handlerType in handlerTypes)
+        {
+            services.AddTransient(handlerType.handlerInterface, handlerType.type);
+        }
+    }
 }
